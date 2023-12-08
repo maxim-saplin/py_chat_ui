4f5e6c0eb84d9ee96b8758d130dd16814cd0dd22
Maxim Saplin <smaxmail@gmail.com>
2023-12-08 20:21:33 +0300

    It works!!!

--- chat.py
+++ chat.py
@@ -4,98 +4,203 @@ from enum import Enum
 from logic import *
 from ui_helpers import *
 
-client = get_client()
-
-#### UI starts here
-
 # Right align token counter
 right_align_2nd_col()
 hide_streamlit_menu()
-
-if 'openai_model' not in st.session_state:
-    st.session_state['openai_model'] = env_model
-
-if 'messages' not in st.session_state:
-    st.session_state.messages = []
 
 class MenuOptions(Enum):
     NEW = 'New'
     SETTINGS = 'Settings'
 
+if 'system_message' not in st.session_state:
+    st.session_state['system_message'] = 'You are an AI assistant capable of chained reasoning as humans do'
+if 'prompt' not in st.session_state:
+    st.session_state['prompt'] = ''
+if 'chat_session_id' not in st.session_state:
+    st.session_state['chat_session_id'] = None
+if 'selected_model_name' not in st.session_state:
+    model = model_repository.get_last_used_model()
+    st.session_state['selected_model_name'] = model.name if model else None
+
+def getModel() -> Model | None:
+    if st.session_state['selected_model_name']:
+        return model_repository.get_model_by_name(st.session_state['selected_model_name'])
+    return None
+
+def get_client() -> OpenAI | AzureOpenAI:
+    return create_client(getModel())
+
+session = None
+if st.session_state['chat_session_id'] is not None:
+    session = session_manager.get_session_by_id(st.session_state['chat_session_id'])    
+
+if 'selected_menu' not in st.session_state:
+    if st.session_state['chat_session_id'] is not None:
+        st.session_state['selected_menu'] = f'{session.title} ({session.session_id})'
+    else:
+        st.session_state['selected_menu'] = MenuOptions.NEW.value if model_repository.models else MenuOptions.SETTINGS.value
+
 with st.sidebar:
-    selected = option_menu(None, [MenuOptions.NEW.value, 'Some chat 1', 'Some chat 2', MenuOptions.SETTINGS.value], 
-        icons=['plus', '','', 'gear'], menu_icon='cast', default_index=1)
+    sessions = session_manager.list_sessions()
+    session_names = [f'{session.title} ({session.session_id})' for session in sessions]
+    menu_options = [MenuOptions.NEW.value] + session_names + [MenuOptions.SETTINGS.value] if getModel() else session_names + [MenuOptions.SETTINGS.value]
+    selected_index = menu_options.index(st.session_state['selected_menu'])
+    previous_menu = st.session_state['selected_menu']
+    st.session_state['selected_menu'] = option_menu(None, menu_options, 
+        icons=['plus'] + [''] * len(session_names) + ['gear'], menu_icon='cast', default_index=0)#manual_select=selected_index)
+    if st.session_state['selected_menu'] in [MenuOptions.NEW.value, MenuOptions.SETTINGS.value]:
+        st.session_state['chat_session_id'] = None
+        session = None
+    else:
+        session_title, session_id_str = st.session_state['selected_menu'].rsplit(' (', 1)
+        session_id = int(session_id_str.rstrip(')'))
+        st.session_state['chat_session_id'] = session_id
+        session = session_manager.get_session_by_id(st.session_state['chat_session_id'])
+    # if previous_menu != st.session_state['selected_menu']:
+    #     st.rerun()
 
 col1, col2= st.columns(2)
 
-if selected not in [MenuOptions.NEW.value, MenuOptions.SETTINGS.value]:
+if st.session_state['selected_menu'] not in [MenuOptions.NEW.value, MenuOptions.SETTINGS.value]:
     with col1:
         st.title('Chat')
 
     with col2:
-        tokens = num_tokens_from_messages(st.session_state.messages)
-        st.write(tokens)
+        if session is not None:
+            tokens = num_tokens_from_messages(session.messages)
+            st.write(tokens)
 
-for message in st.session_state.messages:
-    with st.chat_message(message['role']):
-        st.markdown(message['content'])
+if session != None:
+    for message in session.messages:
+        with st.chat_message(message['role']):
+            st.markdown(message['content'])
 
-temperature = env_temperature if env_temperature else 0.7
-system_message = 'You are an AI assistant capable of chained reasoning as humans do'
-prompt = ''
-api_type = env_api_type if env_api_type else 'Azure' # other option is "OpenAI"
+def get_ai_reply(client: OpenAI | AzureOpenAI, model: Model, session: ChatSession, prompt: str | None) -> str:
+    message_placeholder = st.empty()
+    full_response = ''
 
-#### Starting a chat
-if selected == MenuOptions.NEW.value:
-    model_selected = st.selectbox('Model', ('(env): ' + env_model, 'Cust model 2'))
-    system_message = st.text_area('System message', system_message)
-    temperature= st.slider('Temperature', 0.0, 1.0, temperature, 0.01)
-    prompt = st.text_area('Prompt', prompt)
-    st.button("Send message", type="primary")
+    if prompt != None:
+        session.add_message({'role': 'user', 'content': prompt})
+    for response in client.chat.completions.create(
+                model=model.name,
+                temperature=model.temperature,
+                messages=[
+                    {'role': m['role'], 'content': m['content']}
+                    for m in session.messages
+                ],
+                stream=True,
+            ):
+        if response.choices:
+            first_choice = response.choices[0]
+            if first_choice.delta and first_choice.delta.content:
+                full_response += first_choice.delta.content
+        message_placeholder.markdown(full_response + '▌')
+    message_placeholder.markdown(full_response)
+    session.add_message({'role': 'assistant', 'content': full_response})
+    return full_response
+
 #### Settings
-elif selected == MenuOptions.SETTINGS.value:
-    st.header('Manage Models')
-    model_selected = st.selectbox('Select Model', ['Add New Model'] + [model.name for model in model_repository.models])
-    is_env_model = model_selected.startswith('(env)')
-    is_new_model = model_selected == 'Add New Model'
+if st.session_state['selected_menu'] == MenuOptions.SETTINGS.value:
+    st.title('Manage Models')
+    model_options = ['Add New Model'] + [model.name for model in model_repository.models]
+    model_selected_index = model_options.index(st.session_state['selected_model_name']) if 'selected_model_name' in st.session_state and st.session_state['selected_model_name'] in model_options else 0
+    model_selected_name = st.selectbox('Select Model', model_options, index=model_selected_index)
+    selected_model = next((model for model in model_repository.models if model.name == model_selected_name), None)
+    is_env_model = selected_model.is_env if selected_model else False
+    
+    is_new_model = model_selected_name == 'Add New Model'
     st.subheader('Add New Model' if is_new_model else 'Environment Model Settings' if is_env_model else 'Custom Model Settings')
-    name = st.text_input('Model Name', value=model_selected if not is_new_model else '', disabled=not is_new_model)
-    api_key = st.text_input('API Key', env_api_key, disabled=is_env_model)
-    api_type = st.selectbox('API Type', [option.value for option in ApiTypeOptions], disabled=is_env_model)
-    api_version = st.text_input('API Version', env_api_version, disabled=is_env_model)
-    api_base = st.text_input('API Base', env_api_base, disabled=is_env_model)
-    temperature = st.slider('Temperature', 0.0, 1.0, 0.7, 0.01, disabled=is_env_model)
-    if is_new_model and st.button('Add Model'):
-        model_repository.add(Model(name, api_key, api_type, api_version, api_base, temperature))
-        model_selected = st.selectbox('Select Model', ['Add New Model'] + [model.name for model in model_repository.models]) # Update the model list after adding a new model
+
+    api_type_options = [option.value for option in ApiTypeOptions]
+    api_type_index = api_type_options.index(selected_model.api_type) if selected_model else 0
+    api_type = st.selectbox('API Type', api_type_options, index=api_type_index, disabled=is_env_model, on_change=lambda: st.rerun())
+    is_openai_type = api_type == ApiTypeOptions.OPENAI.value
+
+    name = st.text_input('Model Name', value=model_selected_name if not is_new_model else '', disabled=is_env_model)
+    api_key = st.text_input('API Key', selected_model.api_key if selected_model else '', disabled=is_env_model)
+    api_version = st.text_input('API Version', selected_model.api_version if selected_model else '', disabled=is_env_model or is_openai_type)
+    api_base = st.text_input('API Base', selected_model.api_base if selected_model else '', disabled=is_env_model or is_openai_type)
+    temperature = st.slider('Temperature', 0.0, 1.0, selected_model.temperature if selected_model else 0.7, 0.01, disabled=is_env_model)
+
+    # Validation summary
+    errors = []
+    if not name and not is_env_model and is_new_model:
+        errors.append('Model Name is required.')
+    if not api_key and not is_env_model:
+        errors.append('API Key is required.')
+    if not api_version and not is_env_model and not is_openai_type:
+        errors.append('API Version is required.')
+    if not api_base and not is_env_model and not is_openai_type:
+        errors.append('API Base is required.')
+    if temperature is None and not is_env_model:
+        errors.append('Temperature is required.')
+
+    if errors:
+        for error in errors:
+            st.error(error)
+    
+    if is_new_model:
+        if st.button('Add Model') and not errors:
+            model = Model(name, api_key, api_type, api_version, api_base, temperature)
+            model_repository.add(model)
+            st.success('Model added successfully!')
+            st.session_state['selected_model_name'] = model.name 
+            st.rerun()
+    else:
+        if not is_env_model:
+            if st.button('Update Model') and not errors:
+                selected_model.name = name
+                selected_model.api_key = api_key
+                selected_model.api_type = api_type
+                selected_model.api_version = api_version
+                selected_model.api_base = api_base
+                selected_model.temperature = temperature
+                model_repository.update(selected_model)
+                st.session_state['selected_model_name'] = selected_model.name 
+                st.success('Model updated successfully!')
+                st.rerun()
+            if st.button('Delete Model'):
+                model_repository.delete(selected_model.name)
+                st.success('Model deleted successfully!')
+                last_model = model_repository.get_last_used_model()
+                st.session_state['selected_model_name'] = last_model.name if last_model else None
+                st.rerun()
+            
+#### Starting a chat
+elif st.session_state['selected_menu'] == MenuOptions.NEW.value:
+    st.title('New Chat')
+    model_options = [model.name for model in model_repository.models]
+    selected_model_index = model_options.index(st.session_state['selected_model_name']) if st.session_state['selected_model_name'] in model_options else 0
+    st.session_state['selected_model_name'] = st.selectbox('Model', model_options, index=selected_model_index)
+    st.session_state['system_message'] = st.text_area('System message', st.session_state['system_message'])
+    temperature = st.slider('Temperature', 0.0, 1.0, getModel().temperature, 0.01)
+    prompt = st.text_area('Prompt', st.session_state['prompt'])
+    st.button("Send message", type="primary")
+    if prompt:
+        title = prompt.split()[:5]
+        model = getModel()
+        session = session_manager.create_session(model, ' '.join(title))
+        if st.session_state['system_message']:
+            session.add_message({'role': 'system', 'content': st.session_state['system_message']})
+        session.add_message({'role': 'user', 'content': prompt}) 
+        st.session_state['selected_menu'] = f'{session.title} ({session.session_id})'
+        st.session_state['chat_session_id'] = session.session_id
+        model.temperature = temperature
+        model_repository.update(model)
         st.rerun()
-    elif not is_env_model and st.button('Delete Model'):
-        model_repository.delete(model_selected)
-        model_selected = st.selectbox('Select Model', ['Add New Model'] + [model.name for model in model_repository.models]) # Update the model list after deleting a model
-        st.rerun()
-#### CHAT
+#### Chat
 else:
+    if session.messages and session.messages[-1]['role'] == 'user':
+        with st.chat_message('assistant'):
+            full_response = get_ai_reply(get_client() , getModel(), session, None)
+        #session.add_message({'role': 'assistant', 'content': full_response})
+        st.rerun()
     if prompt := st.chat_input('What is up?'):
-        st.session_state.messages.append({'role': 'user', 'content': prompt})
+        session.add_message({'role': 'user', 'content': prompt})
         with st.chat_message('user'):
             st.markdown(prompt)
 
         with st.chat_message('assistant'):
-            message_placeholder = st.empty()
-            full_response = ''
-            for response in client.chat.completions.create(
-                model=st.session_state['openai_model'],
-                messages=[
-                    {'role': m['role'], 'content': m['content']}
-                    for m in st.session_state.messages
-                ],
-                stream=True,
-            ):
-                if response.choices:
-                    first_choice = response.choices[0]
-                    if first_choice.delta and first_choice.delta.content:
-                        full_response += first_choice.delta.content
-                message_placeholder.markdown(full_response + '▌')
-            message_placeholder.markdown(full_response)
-        st.session_state.messages.append({'role': 'assistant', 'content': full_response})
+            full_response = get_ai_reply(get_client() , getModel(), session, prompt)
+        #session.add_message({'role': 'assistant', 'content': full_response})
         st.rerun()
--- logic.py
+++ logic.py
@@ -1,97 +1,216 @@
 from openai import OpenAI, AzureOpenAI
 import os
+import datetime
 from enum import Enum
 from dotenv import load_dotenv
 import tiktoken
 import pickle
+import glob
 
 load_dotenv()
+
+DATA_FOLDER: str = ".data"
+MODELS_FILE_NAME: str = "models.dat"
+CHATS_FOLDER: str = "chats"
 
 class ApiTypeOptions(Enum):
     AZURE = 'Azure'
     OPENAI = 'OpenAI'
 
-env_api_key = os.environ.get("OPENAI_API_KEY")
-env_api_type = os.environ.get("API_TYPE")
+env_api_key: str | None = os.environ.get("OPENAI_API_KEY")
+env_api_type: str | None = os.environ.get("API_TYPE")
 if env_api_type not in ApiTypeOptions.__members__:
-    env_api_type = ApiTypeOptions.AZURE.value
+    env_api_type: str = ApiTypeOptions.AZURE.value
 else:
-    env_api_type = ApiTypeOptions[env_api_type].value
-env_api_version = os.environ.get("API_VERSION")
-env_api_base = os.environ.get("OPENAI_API_BASE")
-env_model = os.environ.get("MODEL")
-env_temperature = float(os.environ.get("TEMPERATURE"))
+    env_api_type: str = ApiTypeOptions[env_api_type].value
+env_api_version: str | None = os.environ.get("API_VERSION")
+env_api_base: str | None = os.environ.get("OPENAI_API_BASE")
+env_model_name: str | None = os.environ.get("MODEL")
+env_temperature: float = float(os.environ.get("TEMPERATURE", "0.0"))
 
 class Model:
-    def __init__(self, name, api_key, api_type, api_version, api_base, temperature):
-        self.name = name
-        self.api_key = api_key
-        self.api_type = api_type
-        self.api_version = api_version
-        self.api_base = api_base
-        self.temperature = temperature
+    def __init__(self, name: str, api_key: str, api_type: str, api_version: str, api_base: str, temperature: float, is_env: bool = False):
+        self.name: str = name
+        self.api_key: str = api_key
+        self.api_type: str = api_type
+        self.api_version: str = api_version
+        self.api_base: str = api_base
+        self.temperature: float = temperature
+        self.is_env: bool = is_env
 
 class ModelRepository:
     def __init__(self):
-        self.models = []
-        if env_model:
-            self.models.append(Model('(env) ' + env_model, env_api_key, env_api_type, env_api_version, env_api_base, env_temperature))
+        self.models: list[Model] = []
+        self.last_used_model: str = ""
 
-    def load(self):
+    def load(self) -> None:
         try:
-            with open('models.dat', 'rb') as f:
-                self.models = pickle.load(f)
+            with open(os.path.join(DATA_FOLDER, MODELS_FILE_NAME), 'rb') as f:
+                self.models, self.last_used_model = pickle.load(f)
         except FileNotFoundError:
             pass
 
-    def add(self, model):
+    def add_env(self, env_model: str, env_api_key: str, env_api_type: str, env_api_version: str, env_api_base: str, env_temperature: float) -> None:
+        model = Model(env_model, env_api_key, env_api_type, env_api_version, env_api_base, env_temperature, True)
+        self.models.append(model)
+
+    def add(self, model: Model) -> None:
         self.models.append(model)
         self.save()
 
-    def delete(self, model_name):
+    def get_model_by_name(self, model_name: str) -> Model | None:
+        for model in self.models:
+            if model.name == model_name:
+                return model
+        return None
+    
+    def get_last_used_model(self) -> Model | None:
+        if self.last_used_model and self.get_model_by_name(self.last_used_model):
+            return self.get_model_by_name(self.last_used_model)
+        elif self.models:
+            return self.models[0]
+        return None
+    
+    def set_last_used_model(self, model_name: str) -> None:
+        self.last_used_model = model_name
+        self.save()
+
+    def delete(self, model_name: str) -> None:
         self.models = [model for model in self.models if model.name != model_name]
         self.save()
 
-    def update(self, model):
+    def update(self, model: Model) -> None:
         for i, m in enumerate(self.models):
             if m.name == model.name:
                 self.models[i] = model
                 break
         self.save()
 
-    def list(self):
+    def list(self) -> list[str]:
         return [model.name for model in self.models]
 
-    def save(self):
-        with open('models.dat', 'wb') as f:
-            pickle.dump(self.models, f)
+    def save(self) -> None:
+        with open(os.path.join(DATA_FOLDER, MODELS_FILE_NAME), 'wb') as f:
+            pickle.dump((self.models, self.last_used_model), f)
+
 
 model_repository = ModelRepository()
+if env_model_name:
+    model_repository.add_env(env_model_name, env_api_key, env_api_type, env_api_version, env_api_base, env_temperature)
 model_repository.load()
 
+class ChatSession:
+    def __init__(self, session_id: int, model: Model, title: str):
+        self.session_id: int = session_id
+        self.model: Model = model
+        self.title: str = title
+        self.start_date: datetime.datetime = datetime.datetime.now()
+        self.messages: list[dict] = []
+        base_file_path = os.path.join(DATA_FOLDER, CHATS_FOLDER, f"{self.start_date.strftime('%Y%m%d%H%M%S%f')}_{self.session_id}")
+        self.file_path: str = f"{base_file_path}.pkl"
+        self.messages_file_path: str = f"{base_file_path}_messages.pkl"
+        os.makedirs(os.path.dirname(self.file_path), exist_ok=True)
 
-def get_client():
-    return AzureOpenAI(api_key=env_api_key, 
-                     azure_endpoint=env_api_base, 
-                     api_version=env_api_version,
-                     azure_deployment=env_model) if env_api_type == "azure" else OpenAI(api_key=env_api_key)
+    def add_message(self, message: dict) -> None:
+        self.messages.append(message)  # Append the new message to the messages list
+        self.save_message(message)
+
+    def save_message(self, message: dict) -> None:
+        if not os.path.exists(self.file_path):
+            self.save()
+        with open(self.messages_file_path, 'ab') as f:
+            pickle.dump((datetime.datetime.now(), message), f)
+
+    def save(self) -> None:
+        with open(self.file_path, 'wb') as f:
+            data = {
+                'session_id': self.session_id,
+                'model_name': self.model.name,
+                'title': self.title,
+                'start_date': self.start_date
+            }
+            pickle.dump(data, f)
+
+    @classmethod
+    def from_file(cls, file_path: str, model_repository: ModelRepository) -> 'ChatSession':
+        with open(file_path, 'rb') as f:
+            data = pickle.load(f)
+            model_name = data['model_name']
+            model = model_repository.get_model_by_name(model_name)
+            if model is None:
+                model = model_repository.last_used_model
+            chat_session = cls(data['session_id'], model, data['title'])
+            chat_session.start_date = data['start_date']
+            chat_session.file_path = file_path
+            messages_file_path = f"{os.path.splitext(file_path)[0]}_messages.pkl"
+            if os.path.exists(messages_file_path):
+                with open(messages_file_path, 'rb') as mf:
+                    while True:
+                        try:
+                            _, message = pickle.load(mf)
+                            chat_session.messages.append(message)
+                        except EOFError:
+                            break
+            chat_session.messages_file_path = messages_file_path
+            return chat_session
+
+class ChatSessionManager:
+    def __init__(self):
+        self.sessions: list[ChatSession] = []
+        self.load_sessions()
+
+    def create_session(self, model: Model, title: str) -> ChatSession:
+        session_id = max(self.sessions, key=lambda s: s.session_id).session_id + 1 if self.sessions else 1
+        session = ChatSession(session_id, model, title)
+        self.sessions.append(session)
+        return session
+
+    def list_sessions(self) -> list[ChatSession]:
+        return sorted(self.sessions, key=lambda s: s.start_date, reverse=True)
+    
+    def get_session_by_id(self, session_id: int) -> ChatSession | None:
+        for session in self.sessions:
+            if session.session_id == session_id:
+                return session
+        return None
+
+    def load_sessions(self) -> None:
+        chat_files = glob.glob(os.path.join(DATA_FOLDER, CHATS_FOLDER, "*.pkl"))
+        for file_path in chat_files:
+            if not file_path.endswith("_messages.pkl"):
+                try:
+                    session = ChatSession.from_file(file_path, model_repository)
+                    self.sessions.append(session)
+                except (EOFError, pickle.UnpicklingError):
+                    continue
+
+session_manager = ChatSessionManager()
+
+def create_client(model: Model) -> OpenAI | AzureOpenAI:
+    if model.api_type == ApiTypeOptions.AZURE.value:
+        return AzureOpenAI(api_key=model.api_key, 
+                           azure_endpoint=model.api_base, 
+                           api_version=model.api_version,
+                           azure_deployment=model.name)
+    else:
+        return OpenAI(api_key=model.api_key)
 
 encoding = tiktoken.get_encoding("cl100k_base")
 
-def num_tokens_from_messages(messages):
+def num_tokens_from_messages(messages: list[dict]) -> int:
     """Return the number of tokens used by a list of messages."""
 
     if not messages: return 0
 
-    tokens_per_message = 4  # every message follows <|start|>{role/name}\n{content}<|end|>\n
-    tokens_per_name = -1  # if there's a name, the role is omitted
+    tokens_per_message: int = 4  # every message follows <|im_start|>{role/name}\n{content}<|end|>\n
+    tokens_per_name: int = -1  # if there's a name, the role is omitted
 
-    num_tokens = 0
+    num_tokens: int = 0
     for message in messages:
         num_tokens += tokens_per_message
         for key, value in message.items():
             num_tokens += len(encoding.encode(value))
             if key == "name":
                 num_tokens += tokens_per_name
-    num_tokens += 3  # every reply is primed with <|start|>assistant<|message|>
+    num_tokens += 3  # every reply is primed with <|im_start|>assistant<|im_sep|>
     return num_tokens
