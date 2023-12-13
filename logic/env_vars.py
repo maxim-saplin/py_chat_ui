7371478e7bc6a6d5619cc0a7f9c6a81f4e83ed35
Maxim Saplin <smaxmail@gmail.com>
2023-12-13 10:43:39 +0300

    Empy env, Fake model fix

--- logic/env_vars.py
+++ logic/env_vars.py
@@ -4,8 +4,8 @@ from enum import Enum
 
 load_dotenv()
 
-MODELS_FILE_NAME: str = "models.dat"
-CHATS_FOLDER: str = "chats"
+MODELS_FILE_NAME: str = 'models.dat'
+CHATS_FOLDER: str = 'chats'
 
 
 class ApiTypeOptions(Enum):
@@ -22,26 +22,26 @@ class ApiTypeOptions(Enum):
         return None
 
 
-env_api_key: str | None = os.environ.get("OPENAI_API_KEY")
-match os.environ.get("API_TYPE", "Fake").upper():
-    case "AZURE":
+match os.environ.get('API_TYPE', 'Fake').upper():
+    case 'AZURE':
         env_api_type: ApiTypeOptions = ApiTypeOptions.AZURE
-    case "OPENAI":
+    case 'OPENAI':
         env_api_type: ApiTypeOptions = ApiTypeOptions.OPENAI
-    case "FAKE":
+    case 'FAKE':
         env_api_type: ApiTypeOptions = ApiTypeOptions.FAKE
-    case "EMPTY":
+    case 'EMPTY':
         env_api_type: ApiTypeOptions = ApiTypeOptions.EMPTY
     case _:
         None
-env_api_version: str | None = os.environ.get("API_VERSION")
-env_api_base: str | None = os.environ.get("OPENAI_API_BASE")
 env_model_alias: str | None = os.environ.get(
-    "ALIAS",
-    "Fake auto-reply model (demonstration)"
-    if env_api_type == ApiTypeOptions.FAKE else None)
-env_model_name: str | None = os.environ.get("MODEL")
-env_temperature: float = float(os.environ.get("TEMPERATURE", "0.0"))
-env_data_folder: str = os.getenv("DATA_DIR", ".data")
-env_disable_auth: bool = os.environ.get("DISABLE_AUTH", "True").lower() == "true"
-env_disable_user_registration: bool = os.environ.get("DISBLE_USER_REG", "False").lower() == "true"
+    'ALIAS',
+    'Fake auto-reply model (demonstration)'
+    if env_api_type == ApiTypeOptions.FAKE else '(env)')
+env_api_key: str | None = 'Fake' if env_api_type == ApiTypeOptions.FAKE else os.environ.get('OPENAI_API_KEY')
+env_api_version: str | None = 'Fake' if env_api_type == ApiTypeOptions.FAKE else os.environ.get('API_VERSION')
+env_api_base: str | None = 'Fake' if env_api_type == ApiTypeOptions.FAKE else os.environ.get('OPENAI_API_BASE')
+env_model_name: str | None = 'Fake' if env_api_type == ApiTypeOptions.FAKE else os.environ.get('MODEL')
+env_temperature: float = float(os.environ.get('TEMPERATURE', '0.7'))
+env_data_folder: str = os.getenv('DATA_DIR', '.data')
+env_disable_auth: bool = os.environ.get('DISABLE_AUTH', 'True').lower() == 'true'
+env_disable_user_registration: bool = os.environ.get('DISBLE_USER_REG', 'False').lower() == 'true'
